eb726f0b09cd75baaeb5bb96fc7936615ef212a5
enzo borg frantz <enzoborgfrantz@enzos-MacBook-Air.local>
2022-01-12 21:32:28 +0100

    add coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import "./App.css";
+import logo from "./collagehole_logo.png";
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.p`
+  color: #eeebe0;
+  margin-top: -50px;
+  font-size: 25px;
+  font-family: monospace;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <img src={logo} height={200} />
+        <Text>is coming soon...</Text>
+      </Header>
     </div>
   );
 }
